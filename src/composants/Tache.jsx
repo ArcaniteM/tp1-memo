9870817f45211770bf15579e530bc508ed059cea
ArcaniteM <e1171462@cmaisonneuve.qc.ca>
2022-04-09 19:06:58 -0400

    fini tp samedi 9/4

--- src/composants/Tache.jsx
+++ src/composants/Tache.jsx
@@ -1,12 +1,40 @@
 import './Tache.scss';
+import { formaterDate } from '../code/date';
+import * as React from 'react';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+
+// import ColorButtons from '../code/buttons';
+
+function ColorButtonsVert() {
+  return (
+    <Stack direction="row" spacing={2}>
+      <Button variant="contained" color="success">
+        Basculer
+      </Button>
+    </Stack>
+  );
+}
+
+function ColorButtonsRouge() {
+  return (
+    <Stack direction="row" spacing={2}>
+      <Button variant="outlined" color="error">
+        Supprimer
+      </Button>
+    </Stack>
+  );
+}
 
 export default function Tache({id, titre, dateModif}) {
   return (
     <div className="Tache">
-      Basculer
+      {/* Basculer */}
+      <ColorButtonsVert />
       <span className="texte">{titre}</span>
-      <span className="date">{dateModif.seconds}</span>
-      Supprimer
+      <span className="date">{formaterDate(dateModif.seconds)}</span>
+      {/* Supprimer */}
+      <ColorButtonsRouge />
     </div>
   );
 }
